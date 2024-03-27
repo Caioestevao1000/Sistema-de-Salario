7a6da75a4dbc655667fa4dfc6e89a8dcdf0096ed
caioestevao1000 <caioestevao_@hotmail.com>
2024-03-27 01:32:01 -0300

    atualização do html. Construção do php, css e readme.

--- README.md
+++ README.md
@@ -1,1 +1,59 @@
-# Sistema-de-Salario
+# Índice 
+
+* [Projeto de Portfólio Pessoal](#projeto-de-portifolio-pessoal)
+* [Introdução](#introdu%C3%A7%C3%A3o)  
+* [Funcionalidades](#funcioanlidades)  
+* [Tecnologia Utilizadas](#tecnologia-utilizadas)  
+* [Fontes Consultadas](#fontes-consultadas)  
+* [Autores](#autores)  
+
+# Sistema-de-Cálculo-Salarial
+
+Projeto criado com base em atividade da atividade de Programação Web II. Escola Social Marista Ir. Acácio - Ensino Médio Técnico.
+![Capa do Projeto](img/capa.jpg)
+
+
+## Introdução
+Em atividade proposta pelo professor Leonardo em sala de aula, tivemos que calcular o salário de funcionarios de uma empresa fictícia, no qual teriamos os valores da meta semanal, mensal e possíveis bônus mensais para aqueles que batessem as metas semanais, em tal atividade receberiamos o nome do usuário/funcionario, as metas semanais que foram batidas pos ele(a) e a mensal. Apartir disso, e das informações passada pela empresa seria feito o cálculo do salário do funcionario.
+
+A empresa definiu o seguinte:
+Todo(a) vendedor(a) tem salário mínimo de 1.927,02 como pagamento base pelos serviços prestados.  
+Todo(a) vendedor(a) tem meta de venda semanal. Essa meta é de 20 mil reais, totalizando 80 mil/mês por vendedor(a).
+Todo(a) vendedor(a) que bater a meta receberá 1% do valor da meta.
+Todo(a) vendedor(a) que ultrapassar as metas semanais, receberão um 5% sobre o excedente da meta semanal.
+Todo(a) vendedor(a) que exceder o volume da meta mensal, receberá uma bonificação, de 10% sobre o valor excedente da meta mensal. Isso NÃO valerá em caso do não cumprimento de ao menos uma das metas semanais.
+
+
+## Funcioanlidades
+O código constitui de alguns cálculos, no qual foi feito a confirmação dos parâmetros definidos pela empresa.
+
+### 1%
+
+
+### 5%
+
+
+### 10%
+
+
+### resultado
+
+
+
+## Tecnologia Utilizadas
+- :heavy_check_mark: [HTML]
+- :heavy_check_mark: [CSS]
+- :heavy_check_mark: [PHP]
+- :heavy_check_mark: [GIT]
+
+
+## Fontes Consultas
+- W3Schools: [www.w3schools.com](https://www.w3schools.com/php/php_looping_foreach.asp)
+
+## Autores
+Aluno: Caio Estevão
+- Github: [github.com](https://github.com/Caioestevao1000)
+
+### Auxílio
+Professor: Leonardo Rocha
+- Github: [github.com](https://github.com/LeonardoRochaMarista)
--- calc.php
+++ calc.php
@@ -9,20 +9,60 @@
 </head>
 <body>
 
-    <?php
-    $nome = $_GET["nome"];
+    <main>
+        <?php
+        //Entrada de dados passados pelo usuário
+        $nome = $_GET["nome"];
 
-    $semana1 = $_GET["semana1"];
-    $semana2 = $_GET["semana2"];
-    $semana3 = $_GET["semana3"];
-    $semana4 = $_GET["semana4"];
+        $smn1 = $_GET["smn1"];
+        $smn2 = $_GET["smn2"];
+        $smn3 = $_GET["smn3"];
+        $smn4 = $_GET["smn4"];
+        $mensal = $_GET["mensal"];
 
-    $mensal = $_GET["mensal"];
+        //array para cálculo com FOREACH
+        $semanas = array($smn1, $smn2, $smn3, $smn4);
 
-    
-    $SalarioFinal = 192702 + $metasemanal + $ExSemanal + $ExMensal
-    //Salário final = Salário mínimo + valor sobre meta semanal + valor sobre o excedente de meta semanal + valor de excedente de meta mensal.
-    ?>
+        ///Metas recebida da atividade
+        $metaS = 20000; 
+        $metaM = 80000;
+
+        //Salário mínimo de acordo com o Piso Regional-PR
+        $salarioFinal = 1927.02;
+
+        //Cálculo para saber se as metas da semana foram maior ou igual á 20.000, caso seja, é adicionado 200 no Salário
+        foreach($semanas as $s){
+            if($s >= $metaS){
+                $salarioFinal += 200;
+            }
+        }
+
+        //Cálculo do excedente semanal 
+        //Pego cada semana e cálculado se houve excedente ou não. Meta semanal=20.000 se igual, não houve excedente, se maior é subtraido por 20.000 e é pego 5% do excedente.
+        foreach($semanas as $s){
+            if($s > $metaS){
+                $salarioFinal += ($s - $metaS) /100 * 5;
+            }
+        }
+
+        //Cálculo para o excedente mensal, se algumas das metas semanais não forem alcançadas não será feito o cálculo do bônus mensal, caso for alcançado segue a mesma lógica.
+        if($smn1 >= $metaS && $smn2 >= $metaS && $smn3 >= $metaS && $smn4 >= $metaS){
+            $salarioFinal += ($mensal - $metaM) /100 * 10 ;
+        }
+
+        //Mensagem final para o usuário, após todos os cálculos é mostrado ao usuário o valor final do salário
+        echo "<h2>$nome seu resultado salarial é</h2>";
+        echo "<strong><p>R$ $salarioFinal</p></strong>";
+
+        //$SalarioFinal = 192702 + $metasemanal + $ExSemanal + $ExMensal;
+        //Salário final = Salário mínimo + valor sobre meta semanal + valor sobre o excedente de meta semanal + valor de excedente de meta mensal.
+        ?>
+
+        <form id="final" action="index.html">
+            <input  type="submit" value="voltar">
+        </form>
+        
+    </main>
     
 </body>
 </html>
